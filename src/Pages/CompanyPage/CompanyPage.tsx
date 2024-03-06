f180c52b345238910b9d0d013d1e957a4eb5182b
Justin Lee <justin.lee26@gmail.com>
2024-03-06 23:07:35 +1100

    Added api call for company profile

--- src/Pages/CompanyPage/CompanyPage.tsx
+++ src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Sidebar from '../../Components/Sidebar/Sidebar'
+import CompanyDashboard from '../../Components/CompanyDashboard/CompanyDashboard'
+import { useParams } from 'react-router'
+import { CompanyProfile } from '../../company'
+import { getCompanyProfile } from '../../api'
 
 interface Props {}
 
 const CompanyPage = (props: Props) => {
+  let { ticker } = useParams();
+  const [company, setCompany] = useState<CompanyProfile>();
+
+  useEffect(() => {
+    const getProfileInit = async () => {
+      const result = await getCompanyProfile(ticker!);
+      setCompany(result?.data[0]);
+    };
+    getProfileInit();
+  }, [ticker]);
   return (
-    <div>CompanyPage</div>
+    <>
+      {company ?
+            <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+              <Sidebar />
+              <CompanyDashboard />
+              <div>{company?.companyName}</div>
+            </div> : <div>Company not found!</div>
+      }
+    </>
   )
 }
 
